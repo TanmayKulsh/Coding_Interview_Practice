b4f7c318ef97ef357b788cf0d53d80de353cf478
TanmayKulsh <tanmaykulsh@gmail.com>
2023-03-21 10:56:29 +0530

    counting of occurence of each alphabets in a string

---
+++ Strings/src/ashokIt/CountOccurenceOfAlphabets.java
@@ -0,0 +1,36 @@
+package ashokIt;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class CountOccurenceOfAlphabets {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Enter the String");
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		
+		char[] strArr = str.toCharArray();
+		HashMap<Character, Integer> counts = new HashMap<Character, Integer>();
+		
+		for(char c: strArr) {
+			if(counts.containsKey(c)) {
+				int ct = counts.get(c);
+				counts.put(c,ct+1);
+			} else {
+				counts.put(c, 1);
+			}
+		}
+		
+		System.out.println(counts);
+		
+		
+		
+		
+		sc.close();
+		
+
+	}
+
+}
